77b2c61820896064023b47537fb614ae95588d7a
Adeonir Kohl <adeonir@gmail.com>
2023-10-04 18:09:02 -0300

    update eslint config and pump packages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,11 +7,7 @@ export const metadata: Metadata = {
   title: 'Ignite Shop',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
