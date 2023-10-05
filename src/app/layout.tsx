31271fbc1e7037827192c53c260e3ea19f76a6ad
Adeonir Kohl <adeonir@gmail.com>
2023-10-05 16:54:17 -0300

    add product to home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <body className={cn('flex flex-col items-start justify-center', inter.className)}>
         <Header />
         {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import tshirtOne from '~/assets/tshirt-1.png'
+import { cn } from '~/utils/classNames'
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold">Home</h1>
+  return (
+    <main className="ml-auto flex min-h-[656px] w-full max-w-[calc(100vw-((100vw-1152px)/2))] items-center justify-start gap-10 px-4">
+      <Link href="" className="group shrink-0 overflow-hidden pr-4">
+        <article className="relative grid content-center rounded-lg bg-gradient-to-b from-teal-500 to-violet-500 p-16">
+          <Image src={tshirtOne} alt="" width={520} height={520} className="object-cover" />
+          <footer
+            className={cn(
+              'absolute inset-x-1 bottom-1 flex items-center justify-between rounded-md bg-zinc-900/70 p-8 opacity-0',
+              '-translate-y-[110%] transition-all duration-300 group-hover:translate-y-0 group-hover:opacity-100',
+            )}
+          >
+            <strong className="text-xl">Camiseta</strong>
+            <span className="text-2xl font-bold text-emerald-500">R$ 80,00</span>
+          </footer>
+        </article>
+      </Link>
+    </main>
+  )
 }
