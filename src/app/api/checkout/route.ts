f75a60c1ddb1c96f33f38188562297576c4355be
Adeonir Kohl <adeonir@gmail.com>
2023-10-06 14:40:10 -0300

    create checkout route

---
+++ src/app/api/checkout/route.ts
@@ -0,0 +1,29 @@
+import { stripe } from '~/utils/stripe'
+
+export async function POST(request: Request) {
+  const body = await request.json()
+  const { id, priceId } = body
+
+  if (!id || !priceId) {
+    return new Response('Invalid request', { status: 400 })
+  }
+
+  const successUrl = `${process.env.BASE_URL}/success`
+  const cancelUrl = `${process.env.BASE_URL}/products/${id}`
+
+  const session = await stripe.checkout.sessions.create({
+    mode: 'payment',
+    line_items: [
+      {
+        quantity: 1,
+        price: priceId,
+      },
+    ],
+    success_url: successUrl,
+    cancel_url: cancelUrl,
+  })
+
+  return new Response(JSON.stringify(session.url), {
+    status: 201,
+  })
+}
