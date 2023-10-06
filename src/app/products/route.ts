ed6ab12f0487c89a5a4b7e44894dd10f96002480
Adeonir Kohl <adeonir@gmail.com>
2023-10-06 08:45:42 -0300

    create products route to fetch data from stripe

---
+++ src/app/products/route.ts
@@ -0,0 +1,18 @@
+import type Stripe from 'stripe'
+
+import { stripe } from '~/utils/stripe'
+
+export async function GET() {
+  const response = await stripe.products.list({
+    expand: ['data.default_price'],
+  })
+
+  const products = response.data.map((product) => ({
+    id: product.id,
+    name: product.name,
+    image: product.images[0],
+    price: (product.default_price as Stripe.Price).unit_amount,
+  }))
+
+  return Response.json({ data: products })
+}
