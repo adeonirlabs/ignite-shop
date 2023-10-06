4400296257ed3afff2f024579441d0b88d3b05c7
Adeonir Kohl <adeonir@gmail.com>
2023-10-06 19:29:32 -0300

    add metadata to product page

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,4 +1,16 @@
+import type { Metadata } from 'next'
+
 import { Details } from '~/features/details'
+
+export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
+  const id = params.id
+  const product = await fetch(`${process.env.BASE_URL}/api/products/${id}`).then((res) => res.json())
+
+  return {
+    title: `${product.name} | Ignite Shop`,
+    description: 'Explore o nosso catálogo de camisetas temáticas e mostre a sua paixão pela informática.',
+  }
+}
 
 export default function Product({ params }: { params: { id: string } }) {
   const id = params.id
