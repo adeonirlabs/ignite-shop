fab45740dcebcac4200e1ba774513d1ff651012d
Adeonir Kohl <adeonir@gmail.com>
2023-10-06 12:17:12 -0300

    create single product api route

---
+++ src/app/api/products/[id]/route.ts
@@ -0,0 +1,21 @@
+import type Stripe from 'stripe'
+
+import { stripe } from '~/utils/stripe'
+
+export async function GET(_: Request, { params }: { params: { id: string } }) {
+  const id = params.id
+
+  const response = await stripe.products.retrieve(id, {
+    expand: ['default_price'],
+  })
+
+  const product = {
+    id: response.id,
+    name: response.name,
+    description: response.description,
+    image: response.images[0],
+    price: (response.default_price as Stripe.Price).unit_amount,
+  }
+
+  return Response.json({ data: product })
+}
