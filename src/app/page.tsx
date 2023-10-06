822397d7ed247a94c1164c36c8a66d13ffb41915
Adeonir Kohl <adeonir@gmail.com>
2023-10-06 18:48:48 -0300

    create carousel feature that is used on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,56 +1,15 @@
-'use client'
-
-import 'swiper/css'
-
-import { Loader } from 'lucide-react'
-import Image from 'next/image'
-import Link from 'next/link'
-import { useEffect, useState } from 'react'
-import { Swiper, SwiperSlide } from 'swiper/react'
-
-import type { Product } from '~/types'
+import { Carousel } from '~/features/carousel'
 import { cn } from '~/utils/classNames'
-import { formatMoney } from '~/utils/formatMoney'
-
-const getData = async () => {
-  const response = await fetch('/api/products')
-  return response.json()
-}
 
 export default function Home() {
-  const [products, setProducts] = useState<Product[]>([])
-
-  useEffect(() => {
-    getData().then((res) => setProducts(res.data))
-  }, [])
-
   return (
-    <main className="ml-auto flex min-h-[656px] w-full max-w-[calc(100vw-((100vw-1152px)/2))] items-center justify-start px-4">
-      <Swiper spaceBetween={40} width={656}>
-        {products.length === 0 && (
-          <div className="grid h-full w-full place-content-center">
-            <Loader className="h-8 w-8 animate-spin text-teal-500" />
-          </div>
-        )}
-        {products.map((product) => (
-          <SwiperSlide key={product.id}>
-            <Link href={`/products/${product.id}`} className="group shrink-0 overflow-hidden" prefetch={false}>
-              <article className="relative grid place-content-center rounded-lg bg-gradient-to-b from-teal-500 to-violet-500 p-16">
-                <Image src={product.image} alt="" width={520} height={520} className="object-cover" />
-                <footer
-                  className={cn(
-                    'absolute inset-x-1 bottom-1 flex items-center justify-between rounded-md bg-zinc-900/70 p-8 opacity-0',
-                    '-translate-y-[110%] transition-all duration-300 group-hover:translate-y-0 group-hover:opacity-100',
-                  )}
-                >
-                  <strong className="text-xl">{product.name}</strong>
-                  <span className="text-2xl font-bold text-teal-500">{formatMoney(product.price)}</span>
-                </footer>
-              </article>
-            </Link>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+    <main
+      className={cn(
+        'ml-auto flex w-full items-center justify-start px-4',
+        'min-h-[656px] max-w-[calc(100vw-((100vw-1152px)/2))]',
+      )}
+    >
+      <Carousel />
     </main>
   )
 }
---
+++ src/features/carousel.tsx
@@ -0,0 +1,53 @@
+'use client'
+import 'swiper/css'
+
+import { Loader } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+import type { Product } from '~/types'
+import { cn } from '~/utils/classNames'
+import { formatMoney } from '~/utils/formatMoney'
+
+export const Carousel = () => {
+  const [products, setProducts] = useState<Product[]>([])
+
+  const getData = async () => {
+    const response = await fetch('/api/products')
+    return await response.json()
+  }
+
+  useEffect(() => {
+    getData().then((res) => setProducts(res.data))
+  }, [])
+
+  return (
+    <Swiper spaceBetween={40} width={656}>
+      {products.length === 0 && (
+        <div className="grid h-full w-full place-content-center">
+          <Loader className="h-8 w-8 animate-spin text-teal-500" />
+        </div>
+      )}
+      {products.map((product) => (
+        <SwiperSlide key={product.id}>
+          <Link href={`/products/${product.id}`} className="group shrink-0 overflow-hidden" prefetch={false}>
+            <article className="relative grid place-content-center rounded-lg bg-gradient-to-b from-teal-500 to-violet-500 p-16">
+              <Image src={product.image} alt="" width={520} height={520} className="object-cover" />
+              <footer
+                className={cn(
+                  'absolute inset-x-1 bottom-1 flex items-center justify-between rounded-md bg-zinc-900/70 p-8 opacity-0',
+                  '-translate-y-[110%] transition-all duration-300 group-hover:translate-y-0 group-hover:opacity-100',
+                )}
+              >
+                <strong className="text-xl">{product.name}</strong>
+                <span className="text-2xl font-bold text-teal-500">{formatMoney(product.price)}</span>
+              </footer>
+            </article>
+          </Link>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  )
+}
