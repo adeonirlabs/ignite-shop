0ca1531066369aabc95eb3d5cba515ba084788b7
Adeonir Kohl <adeonir@gmail.com>
2023-10-06 18:50:18 -0300

    add metadata to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import type { Metadata } from 'next'
+
 import { Carousel } from '~/features/carousel'
 import { cn } from '~/utils/classNames'
+
+export const metadata: Metadata = {
+  title: 'Ignite Shop',
+  description: 'Explore o nosso catálogo de camisetas temáticas e mostre a sua paixão pela informática.',
+}
 
 export default function Home() {
   return (
