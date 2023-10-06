6d7e921ce78ba03f116cb6b56cb3068313a23580
Adeonir Kohl <adeonir@gmail.com>
2023-10-06 14:38:04 -0300

    add base url to env

--- env.d.ts
+++ env.d.ts
@@ -3,6 +3,7 @@ export {}
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+      BASE_URL: string
       STRIPE_PUBLIC_KEY: string
       STRIPE_SECRET_KEY: string
     }
--- src/utils/stripe.ts
+++ src/utils/stripe.ts
@@ -2,6 +2,7 @@ import Stripe from 'stripe'
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: '2023-08-16',
+  typescript: true,
   appInfo: {
     name: 'Ignite Shop',
   },
