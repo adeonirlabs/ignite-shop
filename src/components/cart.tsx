68b9ab76e7329ac03d03e6e7c5d970e562ae0c10
Adeonir Kohl <adeonir@gmail.com>
2023-10-07 18:38:18 -0300

    add product items to cart list

--- src/components/cart.tsx
+++ src/components/cart.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { X } from 'lucide-react'
+import { Trash2, X } from 'lucide-react'
 
 import { useOutsideClick } from '~/hooks/click-outside'
 import { cn } from '~/utils/classNames'
@@ -24,8 +24,56 @@ export const Cart = ({ open, onClose }: Props) => {
       <button className="ml-auto" type="button" onClick={onClose}>
         <X />
       </button>
-      <div>
-        <h1>Seu carrinho está vazio</h1>
+      <div className="flex flex-col gap-6">
+        <h1 className="mb-4 text-xl font-bold">Sacola de compras</h1>
+        <article className="hover: hover: hover: flex items-start gap-4 rounded-md transition-all hover:bg-zinc-900/30 hover:ring-8 hover:ring-zinc-900/30">
+          <picture className="grid h-24 w-24 shrink-0 place-content-center rounded-lg bg-gradient-to-b from-teal-500 to-violet-500 p-4">
+            {/* <Image src={product.image} alt="" width={220} height={220} className="object-cover" /> */}
+          </picture>
+          <div className="flex w-full flex-col">
+            <span className="text-lg">Camiseta</span>
+            <span className="text-lg font-bold">R$ 79,00</span>
+          </div>
+          <button type="button" className="ml-auto mt-2 p-1 text-teal-600 transition hover:text-teal-500">
+            <Trash2 className="h-5 w-5" />
+          </button>
+        </article>
+        <article className="hover: hover: hover: flex items-start gap-4 rounded-md transition-all hover:bg-zinc-900/30 hover:ring-8 hover:ring-zinc-900/30">
+          <picture className="grid h-24 w-24 shrink-0 place-content-center rounded-lg bg-gradient-to-b from-teal-500 to-violet-500 p-4">
+            {/* <Image src={product.image} alt="" width={220} height={220} className="object-cover" /> */}
+          </picture>
+          <div className="flex w-full flex-col">
+            <span className="text-lg">Camiseta</span>
+            <span className="text-lg font-bold">R$ 79,00</span>
+          </div>
+          <button type="button" className="ml-auto mt-2 p-1 text-teal-600 transition hover:text-teal-500">
+            <Trash2 className="h-5 w-5" />
+          </button>
+        </article>
+        <article className="hover: hover: hover: flex items-start gap-4 rounded-md transition-all hover:bg-zinc-900/30 hover:ring-8 hover:ring-zinc-900/30">
+          <picture className="grid h-24 w-24 shrink-0 place-content-center rounded-lg bg-gradient-to-b from-teal-500 to-violet-500 p-4">
+            {/* <Image src={product.image} alt="" width={220} height={220} className="object-cover" /> */}
+          </picture>
+          <div className="flex w-full flex-col">
+            <span className="text-lg">Camiseta</span>
+            <span className="text-lg font-bold">R$ 79,00</span>
+          </div>
+          <button type="button" className="ml-auto mt-2 p-1 text-teal-600 transition hover:text-teal-500">
+            <Trash2 className="h-5 w-5" />
+          </button>
+        </article>
+        <article className="hover: hover: hover: flex items-start gap-4 rounded-md transition-all hover:bg-zinc-900/30 hover:ring-8 hover:ring-zinc-900/30">
+          <picture className="grid h-24 w-24 shrink-0 place-content-center rounded-lg bg-gradient-to-b from-teal-500 to-violet-500 p-4">
+            {/* <Image src={product.image} alt="" width={220} height={220} className="object-cover" /> */}
+          </picture>
+          <div className="flex w-full flex-col">
+            <span className="text-lg">Camiseta</span>
+            <span className="text-lg font-bold">R$ 79,00</span>
+          </div>
+          <button type="button" className="ml-auto mt-2 p-1 text-teal-600 transition hover:text-teal-500">
+            <Trash2 className="h-5 w-5" />
+          </button>
+        </article>
       </div>
       <button
         type="button"
