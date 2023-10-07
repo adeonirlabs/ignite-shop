2f3b7dcf4d203863ea7823fc9f7ce1ef33255808
Adeonir Kohl <adeonir@gmail.com>
2023-10-07 13:00:33 -0300

    add home link and cart link to header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,21 @@
+import { ShoppingBag } from 'lucide-react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 import logo from '~/assets/logo.svg'
 
 export const Header = () => {
   return (
-    <header className="mx-auto w-full max-w-6xl p-4">
-      <Image src={logo} alt="" width={130} height={52} />
+    <header className="mx-auto flex w-full max-w-6xl items-center justify-between p-4">
+      <Link href="/" className="inline-flex">
+        <Image src={logo} alt="" width={130} height={52} />
+      </Link>
+      <Link href="/cart" className="relative rounded-md bg-zinc-700 p-2 transition hover:bg-zinc-600">
+        <ShoppingBag className="h-8 w-8" />
+        <span className="absolute -right-2.5 -top-2.5 grid h-5 w-5 place-content-center rounded-full bg-teal-500 text-xs">
+          3
+        </span>
+      </Link>
     </header>
   )
 }
